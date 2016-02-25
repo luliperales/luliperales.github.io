af868e3ca209efdc5452d1c10696ad49bced6c0e
luliperales <luliperales@hotmail.com>
2016-02-24 19:13:08 -0500

    code
    commit!!

---
+++ papaya.js
@@ -0,0 +1,13 @@
+var shown = false;
+
+$("#container").click(function() {
+    if (shown == false) {
+    $(".papaya").show();
+    $(".vagina").hide();
+    shown = true;
+    } else {
+    $(".papaya").hide();
+    $(".vagina").show();
+    shown = false;
+    }
+});
